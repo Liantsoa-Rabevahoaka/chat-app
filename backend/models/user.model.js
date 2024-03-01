e781544b61c09b497234d4a277a55853bd061eae
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-03-01 22:19:38 +0300

    add: message and conversation models + add: timestamp to user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,8 +23,9 @@ const userSchema = new mongoose.Schema({
     profilePic:{
         type:String,
         default:"",
-    }
-});
+    },
+    //createdAt, updatedAt => Member since <createdAt>
+}, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
 
