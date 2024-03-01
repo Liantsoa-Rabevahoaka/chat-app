fa48b5a9c8df43b6317a51a48536ac1d9bd047fd
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-03-01 21:28:08 +0300

    add: fonction logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -65,6 +65,7 @@ export const login = async (req, res) => {
             return res.status(400).json({error: "Invalid username ou password"});
         }
 
+        //Connect and define token
         generateTokenAndSetCookie(user._id, res);
 
         res.status(200).json({
@@ -81,5 +82,13 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
+    try {
+        //Elinimate the token
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+        
+    } catch (error) {
+        console.log("Error in logout controller", error.message)
+        res.status(500).json({error: "Internal Server Error"}) 
+    }
 }
