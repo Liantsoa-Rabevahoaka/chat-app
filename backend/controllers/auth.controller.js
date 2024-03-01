510473e9cdacbca6de601d0521c7f3807cf2850a
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-03-01 21:12:20 +0300

    add: fonction login

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -55,8 +55,29 @@ export const signup = async (req, res) => {
     } 
 };  
 
-export const login = (req, res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const {userName, password} = req.body;
+        const user = await User.findOne({userName});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid username ou password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName: user.fullName,
+            userName: user.userName,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+        res.status(500).json({error: "Internal Server Error"}) 
+    }
 }
 
 export const logout = (req, res) => {
