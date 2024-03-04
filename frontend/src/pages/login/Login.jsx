098b23c97a67830ee53f60c8e0062c25ab016db6
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-03-05 00:33:18 +0300

    add: signup UI

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -27,14 +27,13 @@ const Login = () => {
                     </label>
                     <input type="password" placeholder="Enter password" className="w-full input input-bordered h-10" />
                 </div>
+                
                 <a href="#" className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
                     {"Don't"} have an account?
                 </a>
 
                 <div>
-                    <button className="btn btn-block btn-sm mt-2">
-                        Login
-                    </button>
+                    <button className="btn btn-block btn-sm mt-2">Login</button>
                 </div>
 
             </form>
