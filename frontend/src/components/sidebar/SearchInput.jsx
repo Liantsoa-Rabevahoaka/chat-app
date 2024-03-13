a6812a7ba844baa23520c5faa93afabdefbf3a79
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-03-13 21:43:01 +0300

    add: logout button UI

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -12,3 +12,20 @@ const SearchInput = () => {
 }
 
 export default SearchInput
+
+
+//STARTER CODE SNIPPET
+// import { IoSearchSharp } from "react-icons/io5";
+
+// const SearchInput = () => {
+//   return (
+//     <form action="" className="flex items-center gap-2">
+//         <input type="text" placeholder="Search..." className="input input-bordered rounded-full"/>
+//         <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+//           <IoSearchSharp className="w-6 h-6 outline-none"/>
+//         </button>
+//     </form>
+//   )
+// }
+
+// export default SearchInput
