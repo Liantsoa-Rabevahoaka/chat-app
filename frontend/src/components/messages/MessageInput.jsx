75197d35e3c738d16691e83f55dd3c47d2169505
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-03-13 23:03:06 +0300

    fix: the button to the message input

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -3,7 +3,8 @@ import {BsSend} from 'react-icons/bs'
 const MessageInput = () => {
   return (
     <form className="px-4 my-3" action="">
-        <div className='w-full'>
+        {/* We use absolute position so it must be relative*/}
+        <div className='w-full relative'>
             <input type="text" 
                 className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white" 
                 placeholder="Send a message"
